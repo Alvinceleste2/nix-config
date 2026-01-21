9bd4f3016671f713824ecaf22fd1a159b741d3e9
Alvinceleste2 <github.hvgyj15x@alvinceleste.eu>
2026-01-21 19:46:21 +0100

    feat: system default fonts

--- core/fonts.nix
+++ core/fonts.nix
@@ -4,4 +4,10 @@
   fonts.packages = with pkgs; [
     nerd-fonts.fantasque-sans-mono
   ];
+
+  fonts.fontconfig.defaultFonts = {
+    sansSerif = [ "FantasqueSansM Nerd Font Mono"];
+    serif = [ "FantasqueSansM Nerd Font Mono"];
+    monospace = [ "FantasqueSansM Nerd Font Mono"];
+  };
 }
