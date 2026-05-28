91f633db2a16bba7153fde57510808524bd710d2
Alvinceleste2 <github.hvgyj15x@alvinceleste.eu>
2026-05-28 18:47:33 +0200

    feat: user alvinceleste

--- users/alvinceleste.nix
+++ users/alvinceleste.nix
@@ -1,8 +1,29 @@
 { config, pkgs, ... }:
 
 {
-  users.users.alvinceleste = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" "users" "networkmanager" ];
+  nix.settings.trusted-users = [ "alvinceleste" ]
+
+  users = {
+    users = {
+      alvinceleste = {
+        shell = pkgs.zsh;
+        uid = 1000;
+        isNormalUser = true;
+        extraGroups = [
+          "wheel"
+          "users"
+          "networkmanager"
+        ];
+        group = "alvinceleste";
+      };
+    };
+
+    groups = {
+      alvinceleste = {
+        gid = 1000;
+      }
+    };
   };
+
+  programs.zsh.enable = true;
 }
