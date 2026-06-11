11c8a4dd506a2e26db46cb5f54993c9e7f8c6869
Alvinceleste2 <github.hvgyj15x@alvinceleste.eu>
2026-06-12 00:01:05 +0200

    feat: user alvinceleste password with sops

--- users/alvinceleste.nix
+++ users/alvinceleste.nix
@@ -1,6 +1,10 @@
 { config, pkgs, ... }:
 
 {
+  # decrypt alvinceleste-password to /run/secrets-for-users/ so it can be used to create the user
+  sops.secrets.alvinceleste-password.neededForUsers = true;
+  users.mutableUsers = false; # required for password to be set via sops during system activation
+
   nix.settings.trusted-users = [ "alvinceleste" ];
 
   users = {
@@ -9,6 +13,7 @@
         shell = pkgs.zsh;
         uid = 1000;
         isNormalUser = true;
+        hashedPasswordFile = config.sops.secrets.alvinceleste-password.path;
         extraGroups = [
           "wheel"
           "users"
