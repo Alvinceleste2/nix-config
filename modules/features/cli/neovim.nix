3db24df1fd923f0c5e27f57a6f7657c9f06161d9
Álvaro Grande <github.hvgyj15x@alvinceleste.eu>
2026-08-20 10:48:20 +0200

    feat: nixvim keymaps and spelling

--- modules/features/cli/neovim.nix
+++ modules/features/cli/neovim.nix
@@ -12,6 +12,7 @@
       inputs.nixvim.homeModules.nixvim
       self.modules.homeManager.nixvimOptions
       self.modules.homeManager.nixvimSpell
+      self.modules.homeManager.nixvimKeymaps
     ];
 
     programs.nixvim = {
@@ -95,10 +96,6 @@
           msgsep = "‾";
         };
       };
-
-      globals = {
-        spell_enabled = true;
-      };
     };
   };
 
@@ -115,18 +112,178 @@
       keymaps = [
         {
           mode = "n";
-          key = "<leader>ñ";
+          key = "<leader>st";
           action = "<cmd>set spell!<CR>";
           options = {
-            desc = "Toggle spelling";
+            desc = "Toggle Spellcheck";
             silent = true;
           };
+        }
+        {
+          mode = "n";
+          key = "<leader>sf";
+          action = "z=";
+          options.desc = "Fix / Suggestions for word";
+        }
+        {
+          mode = "n";
+          key = "<leader>sa";
+          action = "zg";
+          options.desc = "Add word to dictionary";
+        }
+        {
+          mode = "n";
+          key = "<leader>sr";
+          action = "zw";
+          options.desc = "Remove / Mark word as wrong";
+        }
+        {
+          mode = "n";
+          key = "]s";
+          action = "]s";
+          options.desc = "Next misspelled word";
+        }
+        {
+          mode = "n";
+          key = "[s";
+          action = "[s";
+          options.desc = "Previous misspelled word";
         }
       ];
     };
   };
 
-  flake.modules.homeManager.neovimKeymaps = {
+  flake.modules.homeManager.nixvimKeymaps = {
+    programs.nixvim.keymaps = [
+      # --- Splits ---
+      {
+        mode = "n";
+        key = "<leader>sv";
+        action = "<C-w>v";
+        options.desc = "Split window vertically";
+      }
+      {
+        mode = "n";
+        key = "<leader>sh";
+        action = "<C-w>s";
+        options.desc = "Split window horizontally";
+      }
 
+      # --- Tabs ---
+      {
+        mode = "n";
+        key = "<leader>to";
+        action = "<cmd>tabnew<CR>";
+        options.desc = "Open new tab";
+      }
+      {
+        mode = "n";
+        key = "<leader>tx";
+        action = "<cmd>tabclose<CR>";
+        options.desc = "Close current tab";
+      }
+      {
+        mode = "n";
+        key = "]t";
+        action = "<cmd>tabn<CR>";
+        options.desc = "Go to next tab";
+      }
+      {
+        mode = "n";
+        key = "[t";
+        action = "<cmd>tabp<CR>";
+        options.desc = "Go to previous tab";
+      }
+      {
+        mode = "n";
+        key = "<leader>tf";
+        action = "<cmd>tabnew %<CR>";
+        options.desc = "Open current buffer in new tab";
+      }
+
+      # --- Move lines around with ALT + {j,k} ---
+      {
+        mode = "n";
+        key = "<A-j>";
+        action = ":m +1<CR>==";
+        options.desc = "Move down";
+      }
+      {
+        mode = "n";
+        key = "<A-k>";
+        action = ":m -2<CR>==";
+        options.desc = "Move up";
+      }
+      {
+        mode = "i";
+        key = "<A-j>";
+        action = "<ESC>:m .+1<CR>==gi";
+        options.desc = "Move down";
+      }
+      {
+        mode = "i";
+        key = "<A-k>";
+        action = "<ESC>:m .-2<CR>==gi";
+        options.desc = "Move up";
+      }
+      {
+        mode = "v";
+        key = "<A-j>";
+        action = ":m '>+1<CR>gv=gv";
+        options.desc = "Move down";
+      }
+      {
+        mode = "v";
+        key = "<A-k>";
+        action = ":m '<-2<CR>gv=gv";
+        options.desc = "Move up";
+      }
+
+      # --- Alias awkward movement keys ---
+      {
+        mode = [
+          "n"
+          "v"
+        ];
+        key = "K";
+        action = "{";
+        options.desc = "Go to paragraph backward";
+      }
+      {
+        mode = [
+          "n"
+          "v"
+        ];
+        key = "J";
+        action = "}";
+        options.desc = "Go to paragraph forward";
+      }
+
+      # --- Use Tab in normal and visual modes ---
+      {
+        mode = "n";
+        key = "<Tab>";
+        action = ">>";
+        options.desc = "Indent right";
+      }
+      {
+        mode = "n";
+        key = "<S-Tab>";
+        action = "<<";
+        options.desc = "Indent left";
+      }
+      {
+        mode = "v";
+        key = "<Tab>";
+        action = ">gv";
+        options.desc = "Indent right";
+      }
+      {
+        mode = "v";
+        key = "<S-Tab>";
+        action = "<gv";
+        options.desc = "Indent left";
+      }
+    ];
   };
 }
